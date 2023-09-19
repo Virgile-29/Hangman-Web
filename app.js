fbfe5c9c8c18a2b3692c198888a0420cec460c81
Virgile MOULART <virgile.moulart2022@campus-eni.fr>
2023-09-19 09:46:02 +0200

    bootstrap playground

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import { Game } from "./game.js"
 const startButton = document.getElementById("start")
+const guessButton = document.getElementById("guessButton")
 
+
+const state = {
+    lives: null,
+    word: null,
+    guessedWord: null
+}
 
 startButton.onclick = ((e) => {
+    console.log("START")
     init()
+})
+
+guessButton.onclick = ((e) => {
+   console.log("GUESS")
+    const guessInput = document.getElementById("guessInput").value
+    console.log(guessInput)
 })
 
 const game = new Game()
@@ -19,14 +33,28 @@ function init() {
     // init game
     const rand = Math.floor(Math.random() * words.length)
     const WORD = words[rand]
-    let lives = 7
-    game = new Game(lives, WORD)
+    const lives = 7
+    state.lives = lives
+    state.word = WORD
+    state.guessedWord = 
+    document.getElementById("wordToGuess").innerHTML = WORD
 }
 
 function initHtml() {
     const game = document.getElementById("game")
-
 }
+
+function replaceLettersByUnderscore(word) {
+    mutatedWord = word.replace
+}
+
+function guessLetter(input, word) {
+    
+    for (letters of input) {
+
+    }
+}
+
 
 
 
