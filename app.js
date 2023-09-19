7fba97ec8b7bd2c819b8d5c5067ced612d80f2a2
Virgile MOULART <virgile.moulart2022@campus-eni.fr>
2023-09-19 13:50:12 +0200

    adding API call

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import { Game } from "./game.js"
+import { getWord } from "./words.js"
+
 const startButton = document.getElementById("start")
 const guessButton = document.getElementById("guessButton")
 
@@ -15,44 +16,94 @@ startButton.onclick = ((e) => {
 })
 
 guessButton.onclick = ((e) => {
-   console.log("GUESS")
+
+    console.log("GUESS")
+
     const guessInput = document.getElementById("guessInput").value
-    console.log(guessInput)
+    
+    // Switch to check if input is a character or a string
+    switch (guessInput.length) {
+        case 0:
+            // empty string 💩
+            console.log("💩 Write something, dumb user 💩")
+            break;
+        case 1:
+            // Guessing a letter
+            const guessInputResponse = guessLetter(state.word, state.guessedWord, guessInput)
+            if(!guessInputResponse.isCorrectLetter) {
+                console.log("WRONG LETTER")
+                state.lives = state.lives - 1
+                break;
+            }
+            state.guessedWord = guessInputResponse.guessedWord
+            break;
+        default:
+            // Guessing a word
+            if(guessInput !== state.word) {
+                console.log("Wrong word noob")
+                state.lives = state.lives - 1
+            } else {
+                state.guessedWord = Array.from(guessInput)
+            }
+            break;
+    }
+    console.log("STATE GUESS WORD : ", state.guessedWord)
+    console.log("STATE WORD: ", Array.from(state.word))
+    
+    let wordIsFound = false
+    for(let i = 0 ; i < state.guessedWord.length; i ++) {
+        wordIsFound = state.guessedWord[i] === state.word[i]
+        if(!wordIsFound){
+            break;
+        }
+    }
+
+    if(wordIsFound) {
+        console.log("Congratz ! The word has been found ! 🧑‍🦰")
+    }
+    
+    console.log("input", guessInput)
+    console.log("state: ", state)
+    updateHTML()
 })
 
-const game = new Game()
 
-const words = [
-        "pendu",
-        "jouer",
-        "perdu",
-        "gagner"
-    ]
 
-function init() {
+async function init() {
     // init game
-    const rand = Math.floor(Math.random() * words.length)
-    const WORD = words[rand]
+    const WORD = await getWord()
+
     const lives = 7
     state.lives = lives
-    state.word = WORD
-    state.guessedWord = 
-    document.getElementById("wordToGuess").innerHTML = WORD
+    state.word = WORD[0].name
+    console.log("state.guessedWord", state.guessedWord)
+    state.guessedWord = replaceLettersByUnderscore(WORD[0].name)
+    updateHTML()
 }
 
-function initHtml() {
-    const game = document.getElementById("game")
+function updateHTML() {
+    document.getElementById("wordToGuess").innerHTML = state.guessedWord
+    document.getElementById("lives").innerHTML = `lives : ${state.lives}`
 }
 
 function replaceLettersByUnderscore(word) {
-    mutatedWord = word.replace
+    let guessedWord = []
+    for(let i = 0 ; i < word.length ; i++) {
+        guessedWord.push("_")
+    }
+    return guessedWord
 }
 
-function guessLetter(input, word) {
-    
-    for (letters of input) {
-
+function guessLetter(wordToGuess, currentGuessedWord, guessedLetter) {
+    let guessedWord = currentGuessedWord
+    let isCorrectLetter = false
+    for (let i = 0 ; i < wordToGuess.length ; i++) {
+        if(wordToGuess[i] === guessedLetter) {
+            isCorrectLetter = true
+            guessedWord[i] = guessedLetter
+        }
     }
+    return { guessedWord, isCorrectLetter }
 }
 
 
